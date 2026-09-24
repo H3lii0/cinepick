96cddb1f6f840681e1e0906702abb3defe117548
Helio <helio.miranda.n@gmail.com>
2026-09-24 20:35:48 -0300

    refactor(home): remove unused components and simplify imports

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,26 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { Hero } from './components/hero/hero';
-import { Navbar } from '../../core/layout/navbar/navbar';
-import { WeeklyRail } from './components/weekly-rail/weekly-rail';
-import { TrendingSection } from './components/trending-section/trending-section';
-import { CategorySection } from './components/category-section/category-section';
-import { Footer } from './components/footer/footer';
-import { BottomNav } from '../../core/layout/bottom-nav/bottom-nav';
-import { HomeNavigationService } from '../../services/home-navigation.service';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    Navbar,
-    Hero,
-    WeeklyRail,
-    TrendingSection,
-    CategorySection,
-    Footer,
-    BottomNav,
-  ],
-  providers: [HomeNavigationService],
+  imports: [Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
